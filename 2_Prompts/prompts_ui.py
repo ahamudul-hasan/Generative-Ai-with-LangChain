7e7a57dc779f172f394137774ce5f64a86744a73
Prianto <priantox07@gmail.com>
2026-09-01 18:12:49 +0600

    cmt

--- 2_Prompts/prompts_ui.py
+++ 2_Prompts/prompts_ui.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -19,19 +19,7 @@ style_input = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "Te
 length_input = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"])
 
 # Templete
-template = PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies:
-- Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.""",
-input_variables=['paper_input', 'style_input', 'length_input']
-)
+template = load_prompt('Template.json') 
 
 prompt = template.invoke({
     'paper_input':paper_input,
