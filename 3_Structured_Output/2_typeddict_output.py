ab33f2659ce8fa96bd47d469c92dfc6fc91b84eb
Prianto <priantox07@gmail.com>
2026-09-02 20:54:38 +0600

    cmt

--- 3_Structured_Output/2_typeddict_output.py
+++ 3_Structured_Output/2_typeddict_output.py
@@ -1,14 +1,17 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 
-class Review(TypeError):
-    summary: str
-    sentiment: str
+class Review(TypedDict):
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[str, "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
 
 structured_model = model.with_structured_output(Review)
 
@@ -17,10 +20,25 @@ result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S2
 The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
 
 However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful       
+Review by Moonlight
 """)
 
+print('-------------------------------------------')
 print(result)
+print('----------------')
+print(result['key_themes'])
 print('----------------')
 print(result['summary'])
 print('----------------')
 print(result['sentiment'])
+print('----------------')
+print(result['pros'])
+print('----------------')
+print(result['cons'])
+print('-------------------------------------------')
