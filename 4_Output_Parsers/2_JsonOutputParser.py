8196c5a921d053f5fe2c09a9f663d1e139011233
Prianto <priantox07@gmail.com>
2026-09-03 19:42:04 +0600

    cmt

--- 4_Output_Parsers/2_JsonOutputParser.py
+++ 4_Output_Parsers/2_JsonOutputParser.py
@@ -1,12 +1,29 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+import os
 
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
     repo_id="zai-org/GLM-5.3",
-    task="text-generation"
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 )
 
 model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+templete = PromptTemplate(
+    template="Give me the name, age and city of a fictional person \n {format_instruction}",
+    input_variables=[],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = templete | model | parser
+
+result = chain.invoke({})
+
+print(result)
