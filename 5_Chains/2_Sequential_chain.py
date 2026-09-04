9c56657c3f5ff51914feb13ca99fd95e39e1cd76
Prianto <priantox07@gmail.com>
2026-09-04 13:11:56 +0600

    cmt

---
+++ 5_Chains/2_Sequential_chain.py
@@ -0,0 +1,33 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 detailed report on {topic}',
+    input_variables=['topic']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="zai-org/GLM-5.3",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({'topic': 'Unemployment in Bangladesh'})
+
+print(result)
