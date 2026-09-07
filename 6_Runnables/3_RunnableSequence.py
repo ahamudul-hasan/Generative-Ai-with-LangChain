57aed21e92155fc33a45626e8219c492458b9bb3
Prianto <priantox07@gmail.com>
2026-09-07 16:17:31 +0600

    cmt

---
+++ 6_Runnables/3_RunnableSequence.py
@@ -0,0 +1,30 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_classic.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model='gemini-3.5-flash', 
+    temperature=1.5
+)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+result = chain.invoke({'topic': 'AI'})
+
+print(result)
